2ebdecfcc6cf3d568f10e7fe282c9ca4e8fbab56
Amar Faiz Zainal Abidin <amarf408@gmail.com>
2017-06-14 08:26:08 +0800

    Programming Assignment 2
    Caching the Inverse of a Matrix
    Written by Amar Faiz Zainal Abidin (amarfaiz[at]utem.edu.my)
    Written on 14 June 2017

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix
+## Written by Amar Faiz Zainal Abidin (amarfaiz[at]utem.edu.my)
+## Written on 14 June 2017
 
-## Write a short comment describing this function
+## The main objective of this assignment is to utilize the scoping rules of the R languages for decreasing the computation time of a computed inverse matrix by retriving the available result
+## Similar to example given earlier makeVector, the makeCacheMatrix creates a special "vector" which is a list containing a function to:
+## 1. Set the value of the vector by using variable set
+## 2. Get the value of the vector by using variable get
+## 3. Set the value of the inverse matrix by using variable setInvMat
+## 4. Get the value of the inverse matrix by using variable getInvMat
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+	u <- NULL
+	set <- function(y)
+	{
+		x <<- y
+		u <<- NULL
+	}
+	get <- function() 
+	{
+		x
+	}
+	setInvMat <- function(inverse)
+	{
+		u <<- inverse
+	}
+	getInvMat <- function()
+	{
+		u
+	}
+	list(set = set, get = get, setInvMat = setInvMat, getInvMat = getInvMat)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function check whether the inverse matrix has been computed, If so, it gets the inverse matrix from the cache. Otherwise, it will compute
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+	u <- x$getInvMat()
+	if(!is.null(u))
+	{
+		message("Getting cached data!")
+		return(u)
+	}
+	data <- x$get()
+	u <- solve(data, ...)
+	x$setInvMat(u)
+	u
 }
